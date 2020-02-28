5604652b8f47d3061219e11499093f2cdc954a01
Xuan_the_Coder <wx209dewang@gmail.com>
2020-02-28 16:31:51 -0600

    created controllers for the 5 models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'players/index'
+  resources :players, only: :show
+  resources :clans, only: :show
+  resources :battles, only: :show
+  resources :locations, only: :show
+  resources :decks, only: :show
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'player#index'
 end
---
+++ test/controllers/decks_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DecksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get decks_index_url
+    assert_response :success
+  end
+
+end
