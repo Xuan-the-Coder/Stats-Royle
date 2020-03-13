b0797e1603b094498f396d957e5b40504f9f0b35
Xuan_the_Coder <wx209dewang@gmail.com>
2020-03-13 00:47:11 -0500

    implemented the search form

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,4 +6,8 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
   end
+
+  def search
+    @players = Player.where("name LIKE '%#{params[:search_term]}%'")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :pages
 
   get ":permalink", to: "pages#permalink", as: "permalink"
+
+  get "search", to: "players#search", as: "search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
